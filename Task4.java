07557f0d65bfd39909bab7267d749d0b53c1b1eb
Snake1323 <kirim.kirim53363@gmail.com>
2026-09-10 15:46:40 +0300

    Fix formatting issues in Task4.java

--- Task4.java
+++ Task4.java
@@ -1,8 +1,8 @@
 public class Task4 {
     public static void main(String[] args) {
         char symbol1 = 'J';
-        char symbol2 = 74;       // Код 74 в ASCII — це 'J'
-        char symbol3 = '\u004A'; // Unicode-код для 'J'
+        char symbol2 = 74;       
+        char symbol3 = '\u004A'; 
 
         System.out.println(symbol1);
         System.out.println(symbol2);
